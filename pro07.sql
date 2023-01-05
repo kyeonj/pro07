1d7adc718fe1cbf9aafa771e606422a368f9348f
kyeonj <k_yeonj@naver.com>
2023-01-05 18:34:21 +0900

    rest test

--- pro07.sql
+++ pro07.sql
@@ -6,3 +6,16 @@ create table sample (id varchar(20), pw varchar(20));
 
 insert into sample values('kim','1234');
 insert into sample values('lee','1111');
+
+create table board(seq int primary key,
+title varchar(100) not null,
+content varchar(800) not null,
+nickname varchar(20),
+regdate datetime,
+visited int default 0
+);
+drop table board;
+select * from board;
+
+insert into board values(1, '공지사항1','공지사항1에 대한 내용','admin',sysdate(),0);
+insert into board values(2, '공지사항2','공지사항2에 대한 내용','admin',sysdate(),0);
--- pro07/src/main/java/kr/go/mohw/service/BoardService.java
+++
@@ -1,9 +0,0 @@
-package kr.go.mohw.service;
-
-import java.util.List;
-
-import kr.go.mohw.dto.BoardDTO;
-
-public interface BoardService {
-	public List<BoardDTO> boardList() throws Exception;
-}
